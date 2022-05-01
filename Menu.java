f7d3e7e1436e66664de6a90d0ac5ef5f0d6f16f3
Yvan <dev@yvan.dev>
2022-05-01 23:13:47 +0200

    début du menu listener

--- Menu.java
+++ Menu.java
@@ -14,22 +14,26 @@ public class Menu extends JFrame{
      * Initialisation de la classe Menu
      */
     public Menu(){
+        /* Configuration de la fenêtre */
         this.setTitle("Demineur - Menu");
         this.setVisible(true);
         this.setSize(300,400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /* Ajout des boutons */
         JPanel panel = new JPanel();
         JButton jouer  = new JButton("Nouvelle partie");
-        jouer.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                Game partie = new Game();
-            }
-        });
+        jouer.addActionListener(new MenuListener(this));
         panel.add(jouer);
         JButton charger = new JButton("Charger une partie");
+        charger.addActionListener(new MenuListener(this));
         panel.add(charger);
+
         JButton quitter = new JButton("Quitter");
+        quitter.addActionListener(new MenuListener(this));
         panel.add(quitter);
+
+        /* Ajout des boutons sur la fenêtre */
         this.add(panel);
     }
 }
--- MenuListener.java
+++ MenuListener.java
@@ -1,17 +1,40 @@
 import java.awt.event.*;
 
+import javax.swing.JFrame;
+
 /**
  * La clase <code>MenuListener</code> permet de récupérer les actions effectuées dans le menu.
  * @version 1.0 
  * @author Yvan
  */
 public class MenuListener implements ActionListener{
+    private JFrame parent;
+
+    /**
+     * Initialisation de la classe <code>MenuListener</code>
+     * La classe vient récupérer dans l'initialisation la fenêtre parent qui correspond au menu pour pouvoir la fermer par la suite
+     * @param p fenêtre parent (le menu)
+     * @see Menu
+     */
+    public MenuListener(JFrame p){
+        this.parent = p;         
+    }
     /**
      * Méthode appellée lors d'une action effectuée sur le menu. 
      * Il est possible de savoir sur quel bouton a été effectuée une action.
      * @todo coder les actions pour les boutons du menu
+     * @see Menu
      */
     public void actionPerformed(ActionEvent event){
+        String buttonName = event.getActionCommand();
+        if(buttonName.equals("Nouvelle partie")){
+            this.parent.dispose();
+            Game partie = new Game();
+        } else if(buttonName.equals("Charger une partie")){
+            // On ne fait rien pour l'instant car on n'a pas implémenté la sauvegarde de la partie
+        } else if(buttonName.equals("Quitter")){
+            this.parent.dispose();
+        }
 
     }
 }
