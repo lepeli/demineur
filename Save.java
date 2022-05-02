ad2ab6f9965d1d0462a0e58f382d86360e85baef
Yvan <dev@yvan.dev>
2022-05-02 16:38:39 +0200

    debut de l'ajout des sauvegardes

---
+++ Save.java
@@ -0,0 +1,30 @@
+/**
+ * Save
+ * @author Yvan
+ * @version 1.0
+ */
+public class Save { 
+    /**
+     * Initialisation de la classe Save
+     */
+    public Save(){
+
+    }
+
+    /**
+     * Méthode pour charger une partie sauvegardée.
+     * @return la sauvegarde a bien été chargée ou non
+     */
+    public boolean loadSave(){
+        return true;
+    }
+
+    /**
+     * Méthode pour lancer la sauvegarde d'une partie
+     * @return la sauvegarde a bien été effectuée ou non
+     */
+    public boolean saveGame(){
+        return true;
+    }
+
+}
